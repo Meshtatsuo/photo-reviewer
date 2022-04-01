70bb4d90e66b419325fcf4d4d1ce2d502d090519
meshtatsuo <meshtatsuo@gmail.com>
2022-03-31 18:20:03 -0600

    updated package, updated .env.example

---
+++ public/javascript/upload-file.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#uploadImage");
+
+async function uploadFile(e) {
+    e.preventDefault();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+form.addEventListener('submit', handleSubmit);
--- util/img_upload.js
+++ util/img_upload.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const accessKey = process.env.S3_ACCESSKEY;
 const secretKey = process.env.S3_SECRETKEY;
-const regioin = process.env.S3_REGION;
+const region = process.env.S3_REGION;
 const bucket = process.env.S3_BUCKETNAME;
 
 var S3 = require("aws-sdk/clients/s3");
@@ -16,13 +16,13 @@ const s3 = new S3({
 
 function uploadFile(file) {
   let fileStream = fs.createReadStream(file.path);
-
+  // setup upload paramters S3
   const uploadParams = {
     Bucket: bucket,
     Key: file.filename,
     Body: fileStream,
   };
-
+  // upload file and return promise
   return s3.upload(uploadParams).promise();
 }
 
