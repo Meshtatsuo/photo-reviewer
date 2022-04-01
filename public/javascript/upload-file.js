0580af353a041c7604fc4445283d02d48a0dbc7e
meshtatsuo <meshtatsuo@gmail.com>
2022-04-01 17:35:55 -0600

    removed commented code

--- public/javascript/upload-file.js
+++ public/javascript/upload-file.js
@@ -8,48 +8,16 @@ async function handleSubmit(e) {
     let description = document.getElementById("description").value;
     console.log(file);
 
-
+    // send post request usingi axios as
+    // doing it the standard method was causing
+    // the 'file' to become undefined
     const data = new FormData()
     data.append('image', file)
     data.append('alt_text', alt_text)
     data.append('description', description)
 
     await axios.post('/api/posts/', data);
-
-
-    // if (file && description) {
-    //     const response = await fetch('/api/posts/', {
-    //         method: 'POST',
-    //         body: JSON.stringify({
-    //             filename: file.name,
-    //             path: file.path,
-    //             alt_text: alt_text,
-    //             description: description,
-    //             isApproved: false,
-    //             client_id: 1,
-    //             user_id: 1
-    //         }),
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     });
-
-    //     if (response.ok) {
-    //         document.location.reload();
-    //     } else {
-    //         alert(response.statusText);
-    //     }
-    //}
 }
-
-
-
-
-
-
-
-
-
 
 
 form.addEventListener('submit', handleSubmit);
