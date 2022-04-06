18d59b190702dc57086413348422fd687cd78375
James Hayes <jhayes996@gmail.com>
2022-04-06 18:22:46 -0500

    user routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,4 +3,13 @@ const {
     Comment
 } = require('../../models');
 
+router.get('/', (req,res) => {
+  //route gets all the comments  
+})
+router.get('/:id', (req,res) => {
+//routes gets single comment found by id
+})
+router.post('/', (req,res) => {
+// route create and post comment
+})
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,4 +3,28 @@ const {
     Comment
 } = require('../../models');
 
+router.get('/:id', (req,res) => {
+//Get route for single user excluding password
+const singleUser = {
+    id: req.body.id,
+    username: req.body.username,
+    email: req.body.email
+}
+res.send(`Id:  ${id} Username: ${username} Email: ${email} `);
+})
+
+router.get('/', (req,res) => {
+//Get route for all users excluding password
+const userData = {
+    id: req.body.id,
+    username: req.body.username,
+    email: req.body.email
+}
+res.send(`Id:  ${id} Username: ${username} Email: ${email} `);
+})
+
+router.post('/:id/create', (req,res) => {
+//Post route to create a user
+
+})
 module.exports = router;
